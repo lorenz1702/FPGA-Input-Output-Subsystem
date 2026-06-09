0e8dce6f48986d3a1efd0905ce3d5a0895d8452a
lorenz1702 <112474057+lorenz1702@users.noreply.github.com>
2026-06-09 13:55:42 +0200

    Add UART interrupt service routine and enable interrupt
    Implemented UART interrupt handling and transmission.

--- src/interrupt_io/main.c
+++ src/interrupt_io/main.c
@@ -1,62 +1,44 @@
 #include <xio.h>
 #include <string.h>
 #include "xparameters.h"
-
+#include <mb_interface.h>
 // TODO: Define the UART_BASE_ADDRESS.
 #define UART_BASE_ADDRESS XPAR_MDM_1_BASEADDR
 
-#define UART_RX_FIFO     0x00
-#define UART_TX_FIFO     0x04
-#define UART_STATUS      0x08
-#define UART_CTRL        0x0C
-// Read -- Check Full -- Write Xx
+#define UART_RX_FIFO 	0x00
+#define UART_TX_FIFO 	0x04
+#define UART_STATUS  	0x08
+#define UART_CTRL		0x0C
 
-// Register RX TX STATUS CFG
+// UART CONTROL BITS
+#define UART_CTRL_ENABLE_INTERRUPT 0b00001
+// More information in MicroBlaze product guide!
+void ISR() __attribute__((interrupt_handler));
+u32 count=0;
 
-// TODO: Define the UART register offset. Hint: Look for the register map in the product guide
-#define UART_STATUS_TX_EMPT (1<<2)
-#define UART_STATUS_TX_FULL (1<<3)
-
-// TODO: Define the bit masks for the appropriate status flags. Hint: Look for the status registers in the product guide
-
-u8 is_full(){
-    return XIo_In32(UART_BASE_ADDRESS + UART_STATUS) & 0b1000;
-}
-
-u8 is_empty()
+void ISR(void)
 {
-    return !(XIo_In32(UART_BASE_ADDRESS + UART_STATUS) & 0b1);
+	// TODO: Service the interrupt.
+	u8 value = XIo_In32(UART_BASE_ADDRESS + UART_RX_FIFO);
+	XIo_Out32(UART_BASE_ADDRESS + UART_TX_FIFO, value);
+	count++;
 }
-
-void print(char *s) {
-    // TODO: Write C code to transmit the string using UART.
-    for(int i=0; i<strlen(s); i++)
-    {
-        while(is_full())
-        XIo_Out32(UART_BASE_ADDRESS + UART_TX_FIFO, s[i]);
-    }
-}
-
-// XIo_In32 for input
 
 int main(void) {
-    u32 status = 0;
 
-    char s[] = "Hello World, this is a demonstrator application for polled io!\r\n";
+	// TODO: Enable the UART to generate interrupts
+	XIo_Out32(UART_BASE_ADDRESS + UART_CTRL, UART_CTRL_ENABLE_INTERRUPT);
+	// TODO: Enable microblaze to respond to interrupts
+	microblaze_enable_interrupts();
+	microblaze_register_handler(&ISR, nullptr);
+	while(1) {
 
-    print(s);
+		; // Useful work goes here
 
-    while(1) {
-        // TODO: Write C code to echo received characters.
-        while(is_empty());
-        u8 value = XIo_In32(UART_BASE_ADDRESS + UART_RX_FIFO);
-        XIo_Out32(UART_TX_FIFO, &value);
-    }
+	}
 
-    // Exit normally
-    return 0;
+	// TODO: Disable Interrupts
+
+	// Exit normally
+	return 0;
 }
-
-// Lab goals
-// 1. output
-// 2. Echo --> Write in console, Receive MSg and write back
