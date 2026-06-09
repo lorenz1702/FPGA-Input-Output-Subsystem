13d642909327b23686cdab9046a35a1158624c79
lorenz1702 <112474057+lorenz1702@users.noreply.github.com>
2026-06-09 15:30:03 +0200

    Update ISR for UART RX interrupt handling
    Refactor ISR to handle UART RX interrupt and echo data.

--- src/interrupt_io/main.c
+++ src/interrupt_io/main.c
@@ -10,18 +10,31 @@
 #define UART_STATUS  	0x08
 #define UART_CTRL		0x0C
 
+#define UART_STATUS_RX_VALID 0x01 // Bit 0
+
 // UART CONTROL BITS
-#define UART_CTRL_ENABLE_INTERRUPT 0b00001
+#define UART_CTRL_ENABLE_INTERRUPT 0x10
 // More information in MicroBlaze product guide!
-void ISR() __attribute__((interrupt_handler));
+// void ISR() __attribute__((interrupt_handler));
 u32 count=0;
+
 
 void ISR(void)
 {
-	// TODO: Service the interrupt.
-	u8 value = XIo_In32(UART_BASE_ADDRESS + UART_RX_FIFO);
-	XIo_Out32(UART_BASE_ADDRESS + UART_TX_FIFO, value);
-	count++;
+    // 1. Status-Register read
+    u32 status = XIo_In32(UART_BASE_ADDRESS + UART_STATUS);
+
+	// 2. check if interrupt happend do to rx
+    if (status & UART_STATUS_RX_VALID) {
+        
+        // 3. data read
+        u8 value = XIo_In32(UART_BASE_ADDRESS + UART_RX_FIFO);
+        
+        // 4. (optional) data back as echo
+        XIo_Out32(UART_BASE_ADDRESS + UART_TX_FIFO, value);
+        
+        count++;
+    }
 }
 
 int main(void) {
