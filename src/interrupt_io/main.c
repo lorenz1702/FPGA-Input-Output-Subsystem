cff440e5ea24e11bdfb80dd56df5bff48af148ba
lorenz1702 <112474057+lorenz1702@users.noreply.github.com>
2026-06-10 16:39:11 +0200

    Fix ISR registration and update UART control bits

--- src/interrupt_io/main.c
+++ src/interrupt_io/main.c
@@ -10,14 +10,10 @@
 #define UART_STATUS  	0x08
 #define UART_CTRL		0x0C
 
-#define UART_STATUS_RX_VALID 0x01 // Bit 0
-
 // UART CONTROL BITS
+#define UART_STATUS_RX_VALID 0x01 // Bit 0
 #define UART_CTRL_ENABLE_INTERRUPT 0x10
-// More information in MicroBlaze product guide!
-// void ISR() __attribute__((interrupt_handler));
 u32 count=0;
-
 
 void ISR(void)
 {
@@ -26,13 +22,13 @@ void ISR(void)
 
 	// 2. check if interrupt happend do to rx
     if (status & UART_STATUS_RX_VALID) {
-        
+
         // 3. data read
         u8 value = XIo_In32(UART_BASE_ADDRESS + UART_RX_FIFO);
-        
+
         // 4. (optional) data back as echo
         XIo_Out32(UART_BASE_ADDRESS + UART_TX_FIFO, value);
-        
+
         count++;
     }
 }
@@ -40,10 +36,13 @@ void ISR(void)
 int main(void) {
 
 	// TODO: Enable the UART to generate interrupts
+
 	XIo_Out32(UART_BASE_ADDRESS + UART_CTRL, UART_CTRL_ENABLE_INTERRUPT);
 	// TODO: Enable microblaze to respond to interrupts
+
+
 	microblaze_enable_interrupts();
-	microblaze_register_handler(&ISR, nullptr);
+	microblaze_register_handler(&ISR, 0);
 	while(1) {
 
 		; // Useful work goes here
@@ -51,7 +50,7 @@ int main(void) {
 	}
 
 	// TODO: Disable Interrupts
-
+	//microblaze_disable_interrupts();
 	// Exit normally
 	return 0;
 }
